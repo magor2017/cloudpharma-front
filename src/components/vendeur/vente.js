a024f189abb06439856cd7e396091dc81425fd27
magor2017 <magorsy3@gmail.com>
2019-06-07 21:12:08 +0100

    6-7-2019

--- src/components/vendeur/vente.js
+++ src/components/vendeur/vente.js
@@ -8,48 +8,68 @@ class Vente extends Component {
     constructor(){
         super();
         this.state = {
+            inputValue:true,
             chosedProducts:[],
-            produit:null,
+            produit:{
+                name:"",
+                quantite:1,
+                prix:0
+            },
         }
     }
     
       handleSelectedItem(data){
+          this.hideAutocom();
           let p={
               name:data.name,
               prix:data.prix,
+              quantite:this.state.produit.quantite
           }
           this.setState({produit:p});
-          document.getElementById('q').value=data.prix;
+          console.log(this.state.produit);
+          //document.getElementById('q').value=data.prix;
       }
       addProd(p){
           let prods=this.state.chosedProducts;
-          let q=parseInt(document.getElementById('quantite').value);
-          let some=this.state.chosedProducts.some((el)=>{
-              if(el.name==p.name){
-                return true;
+         let q=this.state.produit.quantite;
+         if(q>0 && !isNaN(q) && q!==undefined && q!==""){
+            let some=this.state.chosedProducts.some((el)=>{
+                if(el.name===p.name){
+                  return true;
+                }
+            });
+            if(some){
+                let products=this.state.chosedProducts;
+                products.forEach((el,index)=>{
+                    if(el.name===p.name){
+                        el.quantite+=q;
+                        this.setState({chosedProducts:products});
+                        this.initPropsProd();
+                    }
+                });
+  
+            }else{
+              if(p.name!=="" && p.name!=undefined && !isNaN(p.prix) && parseInt(p.prix)>0){
+                    let produit=p;
+                    produit.quantite=q
+                    prods.push(produit);
+                    this.setState({chosedProducts: prods});
+                    this.initPropsProd();  
               }
-          });
-          if(some){
-              let products=this.state.chosedProducts;
-              products.forEach((el,index)=>{
-                  if(el.name==p.name){
-                      el.quantite+=q;
-                      this.setState({chosedProducts:products});
-                      
-                  }
-              })
+            }
+         }
+      }
+      initPropsProd(){
+        let prod={
+            name:"",
+            quantite:1,
+            prix:0
+        }
+        this.setState({produit:prod});
 
-          }else{
-            let produit=p;
-            produit.quantite=q
-            prods.push(produit);
-            this.setState({chosedProducts: prods})
-          }
-         
-         // this.state.chosedProducts.push(p);
       }
       deleteProd(p){
-        let produits=this.state.chosedProducts.filter((el,index)=>el.name!=p.name);
+        let produits=this.state.chosedProducts.filter((el,index)=>el.name!==p.name);
         this.setState({chosedProducts:produits});
       }
       totalPrice(){
@@ -63,7 +83,25 @@ class Vente extends Component {
        getItems(item,products){
            return item?matchSorter(products,item,{keys:['name']}):[]
         }
-   
+    handleQuantite(event){
+        event.preventDefault();
+        let qu=parseInt(event.target.value);
+        this.setState({
+            produit:{...{quantite:qu}}
+        });
+        console.log('event!!!');
+    }
+   handleProd(event){
+       event.preventDefault();
+       let qu=event.target.value;
+        this.setState({
+            produit:{name:qu},
+        });
+   }
+   hideAutocom(event){
+       // event.preventDefault();
+        this.setState({auto:false});
+   }
     render() { 
         let products=[
             {id:1,name:"product1",prix:2000},
@@ -73,31 +111,33 @@ class Vente extends Component {
         ]
        
         return ( 
-            <div>
-                <div style={{backgroundColor:"white",color:"#1F838D",textAlign:"center",marginTop:"0.5em",marginBottom:"0.5em"}}>
+            <div onClick={(event)=>this.hideAutocom(event)}>
+                <div id="enteteVente">
                     <div><i class="fas fa-shopping-cart fa-2x"></i></div>
                     <span>Vente</span>
                 </div>
-                <div style={{border:"2px solid white",textAlign:"center"}}>
-                    <div style={{backgroundColor:"white",color:"#1F838D",padding:"10px",width:"15%",marginLeft:"10%",display:"inline-block"}}>
-                        <span><i class="fas fa-shopping-cart fa-2x"></i>Vente</span>
-                    </div>
-                    <div style={{backgroundColor:"#1F838D",color:"white",padding:"10px",width:"15%",marginLeft:"1%",display:"inline-block"}}>
-                        <span><i class="fas fa-history fa-2x"></i>Historique</span>
-                    </div>
-                    <div style={{border:"2px solid white",marginTop:"10px",width:"90%",marginLeft:"auto",marginRight:"auto"}}>
-                        <p style={{backgroundColor:"white",color:"#1F838D"}}>Ajouter un Produit</p>
+                <div id="body" >
+                   <div id="menu">
+                        <div>
+                            <span><i class="fas fa-shopping-cart fa-2x"></i>Vente</span>
+                        </div>
+                        <div>
+                            <span><i class="fas fa-history fa-2x"></i>Historique</span>
+                        </div>
+                   </div>
+                    <div id="ajout">
+                        <p>Ajouter un Produit</p>
                         <div>
                             <div  style={{width:"40%",display:"inline-block"}}>
                                 <label style={{color:"white"}}>Produit</label>
-                                <div style={{display:"inline-block"}}>
-                                   <Downshift>
-                                        {({getLabelProps,getInputProps,isOpen,selectItem,highlightedIndex,inputValue})=>(
+                                <div style={{display:"inline-block"}}>  
+                                    <Downshift >
+                                        {({getLabelProps,getInputProps,isOpen,selectItem,highlightedIndex,inputValue,clearSelection})=>(
                                             <div>
-                                                <input {...getInputProps()} />
-                                                <ul style={{position:"absolute",backgroundColor:"white",listStyleType:"none",paddingLeft:"0px",width:"20%"}}>
+                                                <input {...getInputProps()} onClick={this.state.produit.prix==0?clearSelection:""} />
+                                                <ul id="ulauto">
                                                     {isOpen 
-                                                    ? this.getItems(inputValue,products).map((p,index) => (<li className="liAutocomplete" style={{cursor:"pointer",textAlign:"left"}} key={p.id} onClick={()=>{selectItem(p.name);this.handleSelectedItem(p)}}>{p.name}</li>) 
+                                                    ? this.getItems(inputValue,products).map((p,index) => (<li className="liAutocomplete" key={p.id} onClick={()=>{selectItem(p.name);this.handleSelectedItem(p)}}>{p.name}</li>) 
                                                     ):null}
                                                 </ul>
                                             </div>
@@ -105,13 +145,13 @@ class Vente extends Component {
                                    </Downshift>
                                 </div>
                             </div>
-                            <div style={{width:"25%",display:"inline-block"}}>
-                                <label style={{color:"white"}}>Quantite</label>
-                                <input id="quantite"  type="text" style={{borderRadius:"5px",width:"50%",marginLeft:"15px"}} />
+                            <div className="qp" >
+                                <label>Quantite</label>
+                                <input value={this.state.produit.quantite} onChange={(event)=>this.handleQuantite(event)}  type="number" min="1" />
                             </div>
-                            <div style={{width:"25%",display:"inline-block",}}>
-                                <label style={{color:"white"}}>Prix</label>
-                                <input id="q" type="text" style={{borderRadius:"5px",width:"50%",marginLeft:"15px"}} />
+                            <div className="qp">
+                                <label>Prix</label>
+                                <input value={this.state.produit.prix} type="text" />
                             </div>
                             <div>
                                 <button onClick={()=>this.addProd(this.state.produit)} className="btn btn-success btn-block">Ajouter</button>
@@ -119,9 +159,9 @@ class Vente extends Component {
                         </div>
 
                     </div>
-                    <div style={{width:"90%",marginLeft:"auto",marginRight:"auto",marginTop:"10px"}}>
-                        <table border="2" className="table table-striped table-condensed" style={{border:"1px solid #1F838D"}}>
-                            <thead style={{backgroundColor:"white",color:"#1F838D"}}>
+                    <div className={this.state.chosedProducts.length>0?"tableProds":"tableProdS"} id="tableProds">
+                        <table border="2" className="table table-striped table-condensed" >
+                            <thead >
                                 <tr>
                                     <th>#</th>
                                     <th>Produits</th>
@@ -132,7 +172,7 @@ class Vente extends Component {
                                     <th>Actions</th>
                                 </tr>
                             </thead>
-                            <tbody style={{backgroundColor:"#CFD4FF"}}>
+                            <tbody >
                                 {this.state.chosedProducts.map((p,index)=>{
                                     return(
                                         <tr>
@@ -149,9 +189,9 @@ class Vente extends Component {
                             </tbody>
                         </table>
                     </div>
-                    <div style={{width:"90%",marginLeft:"auto",marginRight:"auto",marginBottom:"10px",backgroundColor:"white"}}>
-                        <div style={{backgroundColor:"#F7DF09",boxShadow:"2px 2px 2px 2px #474747",color:"white",width:"30%",display:"inline-block",marginTop:"10px",marginRight:"10px",marginBottom:"10px"}}><span>Total : {this.totalPrice()}</span></div>
-                        <div style={{width:"20%",display:"inline-block",marginTop:"10px",backgroundColor:"green",color:"white",marginBottom:"10px",boxShadow:"2px 2px 2px 2px #474747"}}><span>Valider</span></div>
+                    <div id="validation" >
+                        <div><span>Total : {this.totalPrice()}</span></div>
+                        <div><span>Valider</span></div>
                     </div>
                 </div>
             </div>
