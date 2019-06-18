d20d6c7da44d59fccb769b7d5dde6a704891d67c
adama0911 <goudiabyadama0911@gmail.com>
2019-06-18 13:17:46 +0000

    ibn bass 18-06-2019 13:16

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Accueil from './components/vendeur/accueil';
+import Accueil  from './components/vendeur/accueil';
+import Login  from './components/login/login';
 import {BrowserRouter as Router,Link,Route,Switch} from 'react-router-dom';
 import { tsConstructorType } from '@babel/types';
 import Content from './components/vendeur/content';
@@ -12,16 +13,52 @@ function App() {
   return (
     <Router>
         <Switch>
-          <Route exact path="/" component={Login} />
+          <Route exact path="/" component={Connexion} />
           <Route exact path="/vendeur" component={Accueil} />
+          <Route exact path="/login" component={Login} />
         </Switch>
     </Router>
   );
 }
-function Login(){
+function Connexion(){
   return(
-    <div>
-        <p>login bi fiila</p>
+    <div className="container">
+        <div className="row">
+          <div className="col-12">
+              <div className="row btncnrow" >
+                <div className="col-12">
+                    <button>CONNEXION</button>
+                </div>
+              </div>
+              <div className="row vrtlinerow" >
+                  <div className="col-6 colon1">
+        
+                  </div>
+                  <div className="col-6 colon2">
+          
+                  </div>
+              </div>
+              <div className="row ">
+                  <div className="col-2 ">
+                  </div >
+                  <div className="col-8 ">
+                      <div className="row srvlinerow">
+                        <div className="col-4">
+                            <span><b>GESTION DE STOCK</b></span>
+                        </div>
+                        <div className="col-4">
+                            <span><b>GESTION DE VENTE</b></span>
+                        </div>
+                        <div className="col-4">
+                            <span><b>GESTION DE CIENTS</b></span>
+                        </div>
+                      </div>
+                  </div>
+              </div>
+          </div>
+        </div>
+        <Link to="/login" >login bi fiila</Link>
+        <br/>
         <Link to="/vendeur" >espace vendeur</Link>
     </div>
     
