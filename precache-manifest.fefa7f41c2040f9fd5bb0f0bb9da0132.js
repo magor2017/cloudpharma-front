157ad9a3f81e539546bdd3c66b05d51439c32841
magor2017 <magorsy3@gmail.com>
2019-07-11 16:37:00 +0100

    Updates

---
+++ precache-manifest.fefa7f41c2040f9fd5bb0f0bb9da0132.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73c76a1bd72af0b502f1f5fb61ef20bf",
+    "url": "/cloudpharma-front/index.html"
+  },
+  {
+    "revision": "93e13250b52f8d2f7c77",
+    "url": "/cloudpharma-front/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "19eb5b8c5e86823131fd",
+    "url": "/cloudpharma-front/static/css/main.cbc72f70.chunk.css"
+  },
+  {
+    "revision": "93e13250b52f8d2f7c77",
+    "url": "/cloudpharma-front/static/js/2.f1cc8e59.chunk.js"
+  },
+  {
+    "revision": "19eb5b8c5e86823131fd",
+    "url": "/cloudpharma-front/static/js/main.9fcfd392.chunk.js"
+  },
+  {
+    "revision": "e7fa122a4c79293358b2",
+    "url": "/cloudpharma-front/static/js/runtime~main.47140bde.js"
+  },
+  {
+    "revision": "e6846cbe028c61e4711df8cc839106bf",
+    "url": "/cloudpharma-front/static/media/login.e6846cbe.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cloudpharma-front/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "617157ebb40ad886d8d86e322179f0e9",
+    "url": "/cloudpharma-front/static/media/password.617157eb.svg"
+  }
+]);
